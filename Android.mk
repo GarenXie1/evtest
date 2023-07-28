72f9a09e6337659d11332a140f5de581ac900b24
garen <longhui.xie@nortekcontrol.com>
2023-07-28 14:03:41 +0800

    1. evtest run done on Android 7.1.2.

---
+++ Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+##############################################
+# evtest Input event tools
+##############################################
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_SRC_FILES  := evtest.c
+LOCAL_MODULE     := evtest
+LOCAL_CFLAGS += -g -O2
+include $(BUILD_EXECUTABLE)
