7e3f13df10e3621534e7f31fcf525bfb5541806e
garen <longhui.xie@nortekcontrol.com>
2023-07-28 15:13:46 +0800

    1. remove warnning as error.

--- Android.mk
+++ Android.mk
@@ -6,5 +6,5 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SRC_FILES  := evtest.c
 LOCAL_MODULE     := evtest
-LOCAL_CFLAGS += -g -O2
+LOCAL_CFLAGS += -g -O2 -Wno-error
 include $(BUILD_EXECUTABLE)
